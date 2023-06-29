1b68785c477de446ee8902c147ec5007470b00e7
Tomasz <tomasz670zielinski@gmail.com>
2023-06-29 18:47:53 +0200

    Java documentation update, and comments for future

--- src/RedirectChecker.java
+++ src/RedirectChecker.java
@@ -8,7 +8,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 
+/**
+ * The RedirectChecker class is used to check redirects of links from a CSV file.
+ */
+
 public class RedirectChecker {
+    /**
+     * The mian method of the program that reads links from CSV file and check for redirects.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         String csvFile = "C:\\Users\\User\\Downloads\\links.csv";
         List<String> links = readLinksFromCSV(csvFile);
@@ -17,6 +25,11 @@ public class RedirectChecker {
         processor.checkMultipleRedirects(links);
     }
 
+    /**
+     * Reads lins from a CSV file witch are separeted by "," and returns them as a list.
+     * @param csvFile the path to the csv file
+     * @return a list of links read from the CSV file
+     */
     public static List<String> readLinksFromCSV(String csvFile) {
         List<String> links = new ArrayList<>();
 
@@ -27,7 +40,7 @@ public class RedirectChecker {
                 links.add(link);
             }
         } catch (IOException e) {
-            System.out.println("Błąd odczytu pliku CSV: " + e.getMessage());
+            System.out.println("An error occurred while reading a CSV file: " + e.getMessage());
         }
 
         return links;
--- src/RedirectProcessor.java
+++ src/RedirectProcessor.java
@@ -3,32 +3,43 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
+/**
+ * The RedirectProcessor class is responsible for checking multiple URLs for redirections.
+ */
 public class RedirectProcessor {
+    /**
+     * The checkMultipleRedirects method is used to checking URL from list.
+     * @param urls The list of URLs
+     */
     public void checkMultipleRedirects(List<String> urls) {
         for (String url : urls) {
             checkRedirect(url);
         }
     }
 
+    /**
+     * Checking single url for redirection
+     * @param url THe URL to be checked
+     */
     public void checkRedirect(String url) {
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setInstanceFollowRedirects(true);
-            connection.setRequestMethod("HEAD");
-            connection.connect();
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection(); //Creating new object and connection to URL. Casting HTTP type.
+            connection.setInstanceFollowRedirects(true); // setting flag for true indicating that connection should fallow redirects and handle them autmatically.
+            connection.setRequestMethod("HEAD"); //HTTP response header, not content.
+            connection.connect(); //establish connection
 
-            int responseCode = connection.getResponseCode();
+            int responseCode = connection.getResponseCode(); //new object for response code
 
-            if (responseCode == HttpURLConnection.HTTP_OK) {
+            if (responseCode == HttpURLConnection.HTTP_OK) { //if response (200)
                 System.out.println("Link: " + url + " Not Redirected");
             } else if (responseCode == HttpURLConnection.HTTP_MOVED_PERM
-                    || responseCode == HttpURLConnection.HTTP_MOVED_TEMP
+                    || responseCode == HttpURLConnection.HTTP_MOVED_TEMP //Check other possibilities
                     || responseCode == HttpURLConnection.HTTP_SEE_OTHER) {
-                String redirectURL = connection.getHeaderField("Location");
+                String redirectURL = connection.getHeaderField("Location"); //redirect URL
                 System.out.println("Link " + url + " redirects to: " + redirectURL);
             }
 
-            connection.disconnect();
+            connection.disconnect(); //closing connection
 
         } catch (IOException e) {
             System.out.println("Error occurred after checking link " + url + ": " + e.getMessage());
